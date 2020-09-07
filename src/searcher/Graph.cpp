98473d936685b9e47076d8b0b4235701e4a82e1e
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-07 21:59:23 +0300

    Updating Graph according to changes

--- src/searcher/Graph.cpp
+++ src/searcher/Graph.cpp
@@ -2,20 +2,13 @@
 
 namespace searcher {
 
-    Graph::Graph(const matrix::Matrix& graphMatrix, const std::pair<uint32_t, uint32_t>& startLocation, const std::pair<uint32_t, uint32_t>& endLocation)
-    : Searchable(GraphElement(startLocation, graphMatrix(startLocation.first, startLocation.second)), 
-    GraphElement(endLocation, graphMatrix(endLocation.first, endLocation.second))), 
+    Graph::Graph(const matrix::Matrix& graphMatrix, const pair& startLocation, const pair& endLocation)
+    : Searchable(graphElement(startLocation, graphMatrix(startLocation.first, startLocation.second)), 
+    graphElement(endLocation, graphMatrix(endLocation.first, endLocation.second))), 
     m_graphMatrix(graphMatrix) {}
 
-    std::vector<Element> Graph::getAllReachableElements(const Element& current) const {
-        
-    }
-
-    Graph::GraphElement::GraphElement(const std::pair<uint32_t, uint32_t>& location, const double value)
-    : m_location(location),
-    m_value(value) {}
-
-    double Graph::GraphElement::getValue() const {
-        return m_value;
+    std::vector<graphElement> Graph::getAllReachableElements(const graphElement& current) const {
+        std::vector<graphElement> reachables;
+        if ()
     }
 }
--- src/searcher/Graph.hpp
+++ src/searcher/Graph.hpp
@@ -7,22 +7,16 @@
 
 namespace searcher {
 
-    class Graph : public Searchable {
+    typedef std::pair<uint32_t, uint32_t> pair;
+    typedef Element<std::pair<uint32_t, uint32_t>, double> graphElement;
+
+    class Graph : public Searchable<pair, double> {
 
         const matrix::Matrix m_graphMatrix;
 
         public:
-            Graph(const matrix::Matrix& graphMatrix, const std::pair<uint32_t, uint32_t>& startLocation, const std::pair<uint32_t, uint32_t>& endLocation);
-            std::vector<Element> getAllReachableElements(const Element& current) const override;
 
-            class GraphElement : public Element {
-
-                const std::pair<uint32_t, uint32_t> m_location;
-                const double m_value;
-
-                public:
-                    GraphElement(const std::pair<uint32_t, uint32_t>& location, const double value);
-                    virtual double getValue() const override;
-            };
+            Graph(const matrix::Matrix& graphMatrix, const pair& startLocation, const pair& endLocation);
+            std::vector<graphElement> getAllReachableElements(const graphElement& current) const override;
     };
 }
