75806b34aa56a7aad919efdf0a220c9991469483
Ilan Prais <ilandovprais@gmail.com>
2020-09-08 21:49:25 +0300

    Adding searcher.cpp class

---
+++ src/searcher/Searcher.cpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Searcher.hpp"
+#include <string>
+
+namespace searcher{
+
+    template <typename IdentifierType, typename ValueType>
+    std::vector<std::string> Searcher::search(const Searchable<IdentifierType, ValueType>& searchable) const {
+
+    }
+}
