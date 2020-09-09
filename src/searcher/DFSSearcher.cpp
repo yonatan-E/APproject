e4cc93bbbe69895ea84587edf83bd9a2c0fe1b62
Ilan Prais <ilandovprais@gmail.com>
2020-09-09 16:59:44 +0300

    updating search

--- src/searcher/DFSSearcher.cpp
+++ src/searcher/DFSSearcher.cpp
@@ -8,16 +8,17 @@ namespace searcher{
     std::vector<std::string> DFSSearcher::search(const Searchable<ElementType>& searchable) const{
         std::vector<ElementType> visited;
         std::vector<std::string> directions;
-        for(searchable.)
-        return recursiveSearch(visited, directions, searchable, searchable.getStartElement());
+        recursiveSearch(visited, directions, searchable, searchable.getStartElement());
     }
 
     template <typename ElementType>
      std::vector<std::string> DFSSearcher::recursiveSearch(std::vector<ElementType>& visited, std::vector<std::string>& directions, const Searchable<ElementType>& searchable, const ElementType& elm){
        
-        if(std::find(visited.begin(), visited.end(), elm) == visited.end()){
+         visited.push_back(elm);
+         for(auto next : elm)
+            if(std::find(visited.begin(), visited.end(), elm) == visited.end()){
 
-        }
+            }
 
      }
 
