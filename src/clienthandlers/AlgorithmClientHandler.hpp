7f6d2d45304c812e7ac5b134b681133deb796f4b
Ilan Prais <ilandovprais@gmail.com>
2020-09-21 12:52:21 +0300

    typo fix

--- src/clienthandlers/AlgorithmClientHandler.hpp
+++ src/clienthandlers/AlgorithmClientHandler.hpp
@@ -5,7 +5,7 @@ namespace handler{
     template <typename Searchable, typename Searcher>
     class AlgorihtmClientHandler : public ClientHandler{
 
-        AlgorihtmClientHandler(const Solver& solver);
+        AlgorithmClientHandler(const Solver& solver);
 
         template <typename int, typename int>
         virtual void handleClient(iStream inputSocket, oStream outputSocket) const;
