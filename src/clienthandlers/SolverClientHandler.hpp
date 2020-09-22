d8f590b9af881623ed3898d1b55a321b27fb27d5
Ilan Prais <ilandovprais@gmail.com>
2020-09-22 10:46:05 +0300

    completed log messages and comments

--- src/clienthandlers/SolverClientHandler.hpp
+++ src/clienthandlers/SolverClientHandler.hpp
@@ -13,37 +13,68 @@ namespace server {
             cache::CacheManager m_cache;
             const int m_bufferSize = 1000000;
             const double m_version = 1.0;
+            const double m_noResponseLength = 0;
+            const double m_successStatus = 0;
+            const int m_NoPathStatus = 0;
+
 
             void handleClient(const int clientSocket) const override{
 
+                //read problem
                 std::string stringProblem = readSock(clientSocket);
+
+                //error in recieving problem
+                if(stringProblem == nullptr){
+                    writeSock(clientSocket, getLog(4, m_noResponseLength));
+                    close(clientSocket);
+                    return;
+                }
+
+                //success in recieving problem
+                writeSock(clientSocket, getLog(m_successStatus, m_noResponseLength));
+
+                //read input
                 std::string stringInput = readSock(clientSocket);
-                std::string result;
+
+                //error in recieving input
+                if(stringInput == nullptr){
+                    writeSock(clientSocket, getLog(5, m_noResponseLength));
+                    close(clientSocket);
+                    return;
+                }
+
+                //success in recieving input, solving the problem with the input
 
                 solver<Problem, Solution> solver& = parseSolver(stringProblem);
 
                 Problem input = solver.parseInput(stringInput);
 
-                //add cache managing
+                //add cache managing here
 
+                std::string result;
+
+                //trying to solve the problem, return error message if one accurs
                 try{
                     result = m_solver.solve(input);
                 }
                 catch(const searcher::exceptions::PathDoesNotExistException&){
-                    result = printLog(1, 0);
+                    result = printLog(1, m_noResponseLength);
                 }
                 catch(const searcher::exceptions::InvalidStartPositionException&){
-                    result =  printLog(2, 0);
+                    result =  printLog(2, m_noResponseLength);
                 }
                  catch(const searcher::exceptions::InvalidEndPositionException&){
-                    result = printLog(3, 0);
+                    result = printLog(3, m_noResponseLength);
                 }
                 catch(const server::exceptions::MessageFormatException&){
-                    result = printLog(4, 0);
+                    result = printLog(4, m_noResponseLength);
                 }
 
+                //success, write a success message and the result to the client
+                writeSock(clientSocket,  getLog(m_successStatus, result.size()));
                 writeSock(clientSocket, result);
 
+                //close the port
                 close(clientSocket);          
             }
 
@@ -58,22 +89,21 @@ namespace server {
 
                 while(bytesRead == read(inputSocket, buffer + messageSize, sizeof(buffer) - messageSize - 1) > 0){
                     messageSize += bytesRead;
-
-                    if(messageSize > bufferSize - 1){
-                        //fail backlog and close connection
+                    if(messageSize > m_bufferSize - 1){
+                        //fail
+                        return nullptr;
                     }
-
                     if(buffer[messageSize - 1] = "\r\n\r\n"){
                         break;
                     }
                 }
 
                 if(bytesRead < 0){
-                    //fail backlog and close connection 
+                    //fail
+                    return nullptr;
                 }
-
-                //Succes backlog
-
+                
+                //success
                 return (std::string) buffer;
             }
 
