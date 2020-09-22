f52b2c8bbd72b80331941e5118ef489b5fe3051a
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-22 22:03:19 +0300

    Updating exception throwing

---
+++ src/parsers/GraphInputParser.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "InputParser.hpp"
+#include "../search/searchable/Graph.hpp"
+
+namespace parser {
+
+    class GraphInputParser : public InputParser<searcher::Graph> {
+
+        public:
+
+            searcher::Graph parseInput(const std::string& input) const;
+    }
+}
