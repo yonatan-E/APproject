c9c508bbf3dface10c6367468bccd0e87dbca782
Ilan Prais <ilandovprais@gmail.com>
2020-09-23 10:34:57 +0300

    starting serial server

---
+++ src/server/SerialServer.cpp
@@ -0,0 +1,13 @@
+#include "SerialServer.hpp"
+
+namespace server{
+
+    void SerialServer::open(uint32_t serverPort, const clientside::SolverClientHandler<searcher::Graph, searcher::SearchResult>& clientHandler) const{
+        
+    }
+
+    void SerialServer::stop() const{
+
+    }
+
+}
---
+++ src/server/SerialServer.hpp
@@ -0,0 +1,15 @@
+#include "Server.hpp"
+#include "../search/searchable/Graph.hpp"
+#include "../search/searcher/SearchResult.hpp"
+#include "../clienthandlers/SolverClientHandler.hpp"
+
+namespace server{
+
+    class SerialServer: public Server<searcher::Graph, searcher::SearchResult>{
+
+        virtual void open(uint32_t serverPort, const clientside::SolverClientHandler<searcher::Graph, searcher::SearchResult>& clientHandler) const;
+
+        virtual void stop() const;
+    };
+    
+}
