f3501e49234812675ee93019ed1e7aa09d86ae13
Ilan Prais <ilandovprais@gmail.com>
2020-09-23 12:39:19 +0300

    changing to client handler

--- src/server/Server.hpp
+++ src/server/Server.hpp
@@ -7,7 +7,7 @@ namespace server{
 
     class Server{
 
-        virtual void open(uint32_t serverPort, const clientside::SolverClientHandler<searcher::Graph, searcher::SearchResult>& clientHandler) const;
+        virtual void open(uint32_t serverPort, const clientside::ClientHandler& clientHandler) const;
 
         virtual bool stop() const;
 
