0ab1fe2dfcfd4e834a4284a33c0fe8e2734b27fa
Ilan Prais <ilandovprais@gmail.com>
2020-09-25 01:12:55 +0300

    final fixes compiles

--- src/clienthandlers/AbstractClientHandler.cpp
+++ src/clienthandlers/AbstractClientHandler.cpp
@@ -1,36 +1,30 @@
 #include "AbstractClientHandler.hpp"
-#include "StatusException.hpp"
+#include "../exceptions/StatusException.hpp"
 #include <unistd.h>
 #include <string.h>
 
-namespace server_side
-{
+namespace server_side {
 
-    namespace client_handler
-    {
+    namespace client_handler {
 
-        std::string AbstractClientHandler::readSock(const uint32_t clientSocket) const
-        {
-
+        std::string AbstractClientHandler::readSock(const uint32_t clientSocket) const {
+                    
             char buffer[s_BUFFER_SIZE];
             int bytesRead = 0;
             int messageSize = 0;
 
             bzero(buffer, s_BUFFER_SIZE);
 
-            while ((bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1)) > 0)
-            {
+            while ((bytesRead == read(clientSocket, buffer, sizeof(buffer) - 1)) > 0) {
 
                 bzero(buffer, s_BUFFER_SIZE);
                 messageSize += bytesRead;
-                if (messageSize > s_BUFFER_SIZE - 1)
-                {
+                if (messageSize > s_BUFFER_SIZE - 1) {
                     break;
                 }
             }
 
-            if (bytesRead < 0)
-            {
+            if (bytesRead < 0) {
                 throw status_exception::StatusException("Failed writing to socket", 6);
             }
 
@@ -39,25 +33,23 @@ namespace server_side
             return message.substr(0, message.size() - 4);
         }
 
-        void AbstractClientHandler::writeSock(const uint32_t clientSocket, const std::string &message) const
-        {
-            if (write(clientSocket, message.c_str(), message.size()) < 0)
-            {
+        void AbstractClientHandler::writeSock(const uint32_t clientSocket, const std::string& message) const {
+            if (write(clientSocket, message.c_str(), message.size()) < 0) {
                 throw status_exception::StatusException("Failed writing to socket", 6);
             }
         }
 
-        void AbstractClientHandler::closeSock(const uint32_t clientSocket) const
-        {
-            if (close(clientSocket) < 0)
-            {
-                throw status_exception::StatusException("Failed closing the socket socket", 6);
+        void AbstractClientHandler::closeSock(const uint32_t clientSocket) const {
+            if (close(clientSocket) < 0) {
+                throw status_exception::StatusException("Failed writing to socket", 6);
             }
         }
 
-        std::string AbstractClientHandler::getLog(const uint32_t version, const uint32_t status, const std::string &message) const
-        {
-            return "Version: " + std::to_string(version) + "\r\n" + "status: " + std::to_string(status) + "\r\n" + "response-length: " + std::to_string(message.size()) + "\r\n" + message + "\r\n";
+        std::string AbstractClientHandler::getLog(const uint32_t version, const uint32_t status, const std::string& message) const {
+            return "Version: " + std::to_string(version) + "\r\n"
+            + "status: " + std::to_string(status) + "\r\n"
+            + "response-length: " + std::to_string(message.size()) + "\r\n"
+            + message + "\r\n";
         }
     }
 }
