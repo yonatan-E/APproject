8244cce12fbfa5828c0a193b6b2a3b77b82fd87d
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-09-24 21:37:40 +0300

    Adding graph

---
+++ build/CMakeFiles/search_tester.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_tester.dir/ErrorCode.cpp.o"
+  "CMakeFiles/search_tester.dir/Graph.cpp.o"
+  "CMakeFiles/search_tester.dir/Matrix.cpp.o"
+  "CMakeFiles/search_tester.dir/SearchResult.cpp.o"
+  "CMakeFiles/search_tester.dir/tester.cpp.o"
+  "search_tester"
+  "search_tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
