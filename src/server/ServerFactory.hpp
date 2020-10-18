2d0525ded36f0daad68cb6a4f499cde19fd3d776
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-10-18 22:50:38 +0300

    comments

--- src/server/ServerFactory.hpp
+++ src/server/ServerFactory.hpp
@@ -8,16 +8,35 @@
 
 namespace server_side {
 
+    /**
+     * @brief This class represents a server factory.
+     * 
+     */
     class ServerFactory {
 
         std::unordered_map<std::string, std::function<std::unique_ptr<Server>()>> m_map;
 
         public:
 
+            /**
+             * @brief Construct a new Server Factory object
+             * 
+             */
             ServerFactory();
 
+            /**
+             * @brief Get a server object according to the type of the server
+             * 
+             * @param type the type of the server
+             * @return std::unique_ptr<Server> a pointer to the server object
+             */
             std::unique_ptr<Server> getServer(const std::string& type) const;
 
+            /**
+             * @brief Get the default server object
+             * 
+             * @return std::unique_ptr<Server> a pointer to the default server object
+             */
             std::unique_ptr<Server> getDefaultServer() const;
     };
 }
