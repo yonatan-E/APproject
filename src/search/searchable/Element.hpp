7f8863771445e51ceb4ea7c1a33fdb29601edc96
Yonatan Ehrenreich <yonatan16.er@gmail.com>
2020-11-02 13:25:38 +0200

    Making Element an Interface

--- src/search/searchable/Element.hpp
+++ src/search/searchable/Element.hpp
@@ -12,69 +12,35 @@ namespace searcher
     class Element
     {
 
-        Identifier m_identifier;
-        double m_value;
-        double m_gScore;
-        double m_hScore;
-
     public:
-        /**
-             * @brief Construct a new Element object
-             * 
-             * @param identifier the identifier of the element
-             * @param value the value of the element
-             */
-        Element(const Identifier &identifier, const double value)
-            : m_identifier(identifier),
-              m_value(value) {}
-
-        /**
-             * @brief Calculate and set the heuristic scores of the element
-             * 
-             * @param startPos the start position of the search
-             * @param endPos the end position of the search
-             */
-        void calculateHeuristics(const Element<Identifier> &startPos, const Element<Identifier> &endPos);
 
         /**
              * @brief Get the Identifier of the element
              * 
              * @return const Identifier& the identifier of the element
              */
-        const Identifier &getIdentifier() const
-        {
-            return m_identifier;
-        }
+        virtual const Identifier &getIdentifier() const;
 
         /**
              * @brief Get the Value of the element
              * 
              * @return double the value of the element
              */
-        double getValue() const
-        {
-            return m_value;
-        }
+        virtual double getValue() const;
 
         /**
              * @brief Get the heuristic G score of the element
              * 
              * @return double the G score of the element, which is the estimated distance to the end position
              */
-        double getGScore() const
-        {
-            return m_gScore;
-        }
+        virtual double getGScore() const;
 
         /**
              * @brief Get the heuristic H score of the element
              * 
              * @return double the H score of the element, which is the distance to the start position
              */
-        double getHScore() const
-        {
-            return m_hScore;
-        }
+        virtual double getHScore() const;
 
         /**
              * @brief Compare operator
@@ -83,10 +49,7 @@ namespace searcher
              * @return true if the elements are equal
              * @return false if the elements are not equal
              */
-        bool operator==(const Element<Identifier> &other) const
-        {
-            return m_identifier == other.m_identifier && m_value == other.m_value;
-        }
+        virtual bool operator==(const Element<Identifier> &other) const;
 
         /**
              * @brief Negative compare operator
@@ -95,10 +58,7 @@ namespace searcher
              * @return true if the elements are not equal
              * @return false if the elements are equal
              */
-        bool operator!=(const Element<Identifier> &other) const
-        {
-            return !(*this == other);
-        }
+        virtual bool operator!=(const Element<Identifier> &other) const;
     };
 
     /**
